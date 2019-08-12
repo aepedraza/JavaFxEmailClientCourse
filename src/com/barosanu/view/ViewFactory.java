311ec11c6099cb28cc9230aa71cebf050c4471e3
barosanuemailtest <barosanu.email.test@gmail.com>
2019-08-12 22:28:01 +0300

    Stage actions

--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -3,6 +3,7 @@ package com.barosanu.view;
 import com.barosanu.EmailManager;
 import com.barosanu.controller.BaseController;
 import com.barosanu.controller.LoginWindowController;
+import com.barosanu.controller.MainWindowController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -28,8 +29,18 @@ public class ViewFactory {
 
         // This routine helps to communicate between controllers dynamically
         BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
-        fxmlLoader.setController(controller);
+        initializeStage(controller);
+    }
+    public void showMainWindow(){
+        System.out.println("main window called");
+
+        BaseController controller = new MainWindowController(emailManager, this, "MainWindow.fxml");
+        initializeStage(controller);
+    }
+
+    private void initializeStage(BaseController baseController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
         Parent parent;
         try { // because FXML file loading can fail
             parent = fxmlLoader.load();
@@ -41,5 +52,9 @@ public class ViewFactory {
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.show();
+    }
+
+    public  void closeStage(Stage stageToClose){
+        stageToClose.close();
     }
 }
