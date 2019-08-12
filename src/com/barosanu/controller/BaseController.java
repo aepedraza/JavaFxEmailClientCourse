25f96d6b6a1b91823e975ddf5f2b877142848be4
barosanuemailtest <barosanu.email.test@gmail.com>
2019-08-12 21:56:24 +0300

    Initialize stages.
    (cherry picked from commit 128923446fcfdd7c57fb28302fcc25462f3f6e54)

--- src/com/barosanu/controller/BaseController.java
+++ src/com/barosanu/controller/BaseController.java
@@ -17,4 +17,8 @@ public abstract class BaseController {
         this.viewFactory = viewFactory;
         this.fxmlName = fxmlName;
     }
+
+    public String getFxmlName() {
+        return fxmlName;
+    }
 }
