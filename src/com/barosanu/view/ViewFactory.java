18d53faffb89d7f7a04b936d5a52e7eb7475d841
Alvaro Pedraza <aepedraza3117@gmail.com>
2020-02-17 16:22:02 -0300

    Introduction to CSS (split 9d7411a)

--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Factory for invoking views.
@@ -18,9 +19,11 @@ import java.io.IOException;
 public class ViewFactory {
 
     private EmailManager emailManager;
+    private ArrayList<Stage> activeStages;
 
     public ViewFactory(EmailManager emailManager) {
         this.emailManager = emailManager;
+        activeStages = new ArrayList<Stage>();
     }
 
     //View options handling:
@@ -77,9 +80,18 @@ public class ViewFactory {
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.show();
+        activeStages.add(stage);
     }
 
     public  void closeStage(Stage stageToClose){
         stageToClose.close();
+        activeStages.remove(stageToClose);
+    }
+
+    public void updateStyles() {
+        for (Stage stage: activeStages) {
+            Scene scene = stage.getScene();
+            // handle the css
+        }
     }
 }
